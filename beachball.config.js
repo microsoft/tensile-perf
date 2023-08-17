3716cd4d286b73d13f71b7bcf606f334d126138b
Sean Monahan <seanmonahan@microsoft.com>
2023-08-17 21:50:11 +0000

    clean up config

---
+++ beachball.config.js
@@ -0,0 +1,20 @@
+// @ts-check
+
+/**
+ * @type {import('beachball').BeachballConfig}
+ */
+module.exports = {
+    gitTags: false,
+    ignorePatterns: [
+      '**/__fixtures__/**',
+      '**/*.test.{ts,tsx}',
+      '**/*.stories.tsx',
+      '**/.eslintrc.json',
+      '**/jest.config.js',
+      '**/project.json',
+      '**/README.md',
+    ],
+    hooks: require('./beachball.hooks'),
+    scopes: ["packages/*"]
+  };
+  
